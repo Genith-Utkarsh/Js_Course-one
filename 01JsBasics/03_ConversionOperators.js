e19f1d6b030bce007af49b497b01e5d52fde338e
Utkarsh Buva <buvautkarsh849@gmail.com>
2025-05-09 13:42:45 +0000

    Data conversions

---
+++ 01JsBasics/03_ConversionOperators.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)         // converting the variable type into number 
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc"  => NaN  ==> Type => Number
+
+//  true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+
+// converting into Boolean
+
+let isLoggedIn = 1
+let boolLogged = Boolean(isLoggedIn)
+
+console.log(boolLogged);
+
+
+//  1 => true
+//  0 => false
+
+// "" => (empty string) => false
+// "Some text"  => true
+

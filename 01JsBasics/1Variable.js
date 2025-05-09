23d9e0fc1d6d5c2122cb906199b80ec60bf15433
Utkarsh Buva <buvautkarsh849@gmail.com>
2025-05-09 13:21:29 +0000

    Variable and Data Types

---
+++ 01JsBasics/1Variable.js
@@ -0,0 +1,17 @@
+const accountId = 123                   // cant redeclare and cant update
+let accountEmail = "Howz@google.com"    // acn be update but cannot redeclare
+var accountPassowrd = "12345"         // avoid using Var 
+
+let accountType;      // undefined
+
+// Updating the values
+
+accountEmail = "HowzThat@google.com"
+accountPassowrd = "123123"
+
+console.table([accountId, accountEmail, accountPassowrd, accountType])
+
+
+/*
+    Multiline comment 
+*/                                                                                  
---
+++ 01JsBasics/2_DataTypes.js
@@ -0,0 +1,14 @@
+let num = 2   // number
+let str = "deku"  // string
+let value = null   // null => standalone value
+let flag = true   // Boolean value => true, false
+
+// undefined
+// symbol
+// Object
+
+// To know Data type of Variable use => typeeof
+
+console.log(typeof null);
+console.log(typeof undefined);
+

3a121d6bf38edbbc50f4d37f55817c203a6c72e9
Utkarsh <buvautkarsh849@gmail.com>
2025-05-12 14:17:21 +0000

    started learning about string

---
+++ 01JsBasics/6string.js
@@ -0,0 +1,15 @@
+// using back tiks
+
+let userName = "Utkarsh"
+let userScore = 100
+
+console.log(`${userName} has score = ${userScore}`)
+
+
+
+// Type of string : Object 
+
+// another way to declare string 
+
+const someSring = new String("OffSec")
+console.log(someSring[0]);
